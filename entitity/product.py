85699b261d45edca131357643a170439c8c9f67f
Vladislav <talcore@yandex.ru>
2024-11-23 21:07:58 +0500

    add: entities

---
+++ entitity/product.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Product:
+
+    name: str
+    full_price: int
+    price_with_discount: int
+    url: str
+    in_stock: int
+
+    def to_dict(self):
+        return self.__dict__
+
+    @classmethod
+    def to_model(cls):
+        return cls.__class__
+
+
+
