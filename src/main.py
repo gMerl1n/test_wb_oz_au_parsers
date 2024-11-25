62cc21329c70f4af56eaa2ce54db9fef46222a9a
Vladislav <talcore@yandex.ru>
2024-11-26 01:29:27 +0500

    add: cookies loader in schedule

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from src.routes import _routes
 from container.di_container import di_container
+from src.services.cookies_service.cookies_loader import oz_loader_cookies
 
 
 app = FastAPI()
@@ -13,8 +14,8 @@ scheduler = AsyncIOScheduler()
 
 @app.on_event('startup')
 async def run_wb_parser():
-    wq = di_container.get_parser_wb()
-    scheduler.add_job(wq.parse_wb, 'cron', minute='*/1')
+    w = di_container.get_parser_wb()
+    scheduler.add_job(w.parse_wb, 'cron', minute='*/5')
     scheduler.start()
 
 
@@ -26,6 +27,13 @@ async def run_oz_parser():
 @app.on_event('startup')
 async def run_au_parser():
     pass
+
+
+@app.on_event('startup')
+async def run_cookies_oz_loader():
+    scheduler.add_job(oz_loader_cookies.load_cookies, 'cron', minute='*/3')
+    scheduler.start()
+
 
 
 
