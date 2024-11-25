e7ee18db64e3aed0fd39a20367209d38f22a26c8
Vladislav <talcore@yandex.ru>
2024-11-26 00:35:43 +0500

    add: handlers

--- src/api/handlers.py
+++ src/api/handlers.py
@@ -1,26 +1,29 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import JSONResponse
-from fastapi import Depends
-from src.services.wb_service import BaseWBParser
+
 from container.di_container import di_container
+from src.use_cases.product_use_cases import BaseUseCasesProduct
+from settings.settings import PARSERS
 
 
 router_product = APIRouter()
 
 
 @router_product.get("/")
-async def run_wb_parser(sign: str,
-                        parser_wb_service: BaseWBParser = Depends(di_container.get_parser_wb)):
-    pass
+async def get_products_by_sign(sign: str,
+                               use_cases_product: BaseUseCasesProduct = Depends(di_container.get_use_cases_product)):
+
+    if sign not in PARSERS:
+        raise HTTPException(status_code=400, detail=f"No products with such a sign: {sign} "
+                                                    f"Try {PARSERS}")
+
+    products_by_sign = use_cases_product.get_products_by_sign(sign=sign)
+    return JSONResponse(status_code=200, content=products_by_sign)
 
 
 @router_product.get("/")
-async def run_oz_parser(sign: str):
-    pass
+async def get_all_products(use_cases_product: BaseUseCasesProduct = Depends(di_container.get_use_cases_product)):
 
-
-@router_product.get("/")
-async def run_au_parser(sign: str):
-    pass
-
+    products = use_cases_product.get_all_products()
+    return JSONResponse(status_code=200, content=products)
 
