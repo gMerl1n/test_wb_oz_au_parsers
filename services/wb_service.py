d02b7e0d6e3b9e2d635a379a6a589b237ba71954
Vladislav <talcore@yandex.ru>
2024-11-25 13:11:52 +0500

    add: logger in WB parser

--- services/wb_service.py
+++ services/wb_service.py
@@ -1,22 +1,19 @@
 import asyncio
 import aiohttp
-
-from abc import abstractmethod, ABC
+import requests
+from logger.logger import init_logger
 from entitity.product import Product
 
 
-class BaseWBParser(ABC):
-
-    @abstractmethod
-    def parse_wb(self):
-        pass
+log = init_logger(__name__)
 
 
-class WBParser(BaseWBParser):
+class WBParser:
+
+    sign: str= "WB"
 
     list_products: list[Product] = []
     list_json_pages: list[dict] = []
-    list_urls: list[str] = []
 
     base_wb_url: str = "https://catalog.wb.ru/"
     page: int = 1
@@ -34,9 +31,21 @@ class WBParser(BaseWBParser):
                             f"catalog?ab_testing=false&appType=1&cat=130404&curr=rub&dest=-5818883&page={self.page}&sort=popular&spp=30")
             return api_url
 
-    def get_page_numbers(self, data_products: dict) -> int | None:
+    def make_request(self) -> requests.Response | None:
 
-        data: dict = data_products.get("data")
+        url: str = self.build_wb_api_url()
+
+        response = requests.get(url)
+        if response.status_code != 200:
+            return
+        else:
+            return response
+
+    def get_page_numbers(self) -> int | None:
+
+        response = self.make_request()
+
+        data: dict = response.json().get("data")
 
         if data is None:
             return
@@ -47,13 +56,20 @@ class WBParser(BaseWBParser):
 
         return int(num_products // self.num_products_on_page)
 
-    def build_urls(self) -> None:
+    def build_urls(self) -> list[str]:
+
+        all_pages = self.get_page_numbers()
+        log.info(f"{self.sign} All pages to parse: {all_pages}")
+
+        list_urls: list[str] = []
 
         for _ in range(1, 4):
 
             api_url = self.build_wb_api_url()
-            self.list_urls.append(api_url)
+            list_urls.append(api_url)
             self.page += 1
+
+        return list_urls
 
     @staticmethod
     def get_products(data_products: dict) -> list[dict] | None:
@@ -69,16 +85,18 @@ class WBParser(BaseWBParser):
 
         return products
 
-    @staticmethod
-    def create_product_object(product_wb: dict) -> Product:
+    def create_product_object(self, product_wb: dict) -> Product:
 
-        return Product(
+        new_product = Product(
             name=product_wb.get("name"),
             full_price=product_wb.get("sizes")[0].get("price").get("basic") // 100,
             price_with_discount=product_wb.get("sizes")[0].get("price").get("product") // 100,
             url=f"https://www.wildberries.ru/catalog/{product_wb.get('id')}/detail.aspx",
             in_stock=product_wb.get("totalQuantity")
         )
+
+        log.debug(f"{self.sign} New product {product_wb.get("name")} added")
+        return new_product
 
     def parse_products(self) -> None:
 
@@ -103,22 +121,22 @@ class WBParser(BaseWBParser):
             self.list_json_pages.append(data)
 
     async def parse_wb(self) -> None:
+
+        list_urls = await asyncio.to_thread(self.build_urls)
+        log.info(f"{self.sign} Number urls with products to parse {len(list_urls)}")
+
         async with aiohttp.ClientSession() as sess:
             tasks: list = []
-            for url in self.list_urls:
+            for url in list_urls:
                 task = asyncio.create_task(self.get_product_data(sess, url))
                 tasks.append(task)
 
             await asyncio.gather(*tasks)
 
-        for pr in self.list_json_pages:
-            await asyncio.to_thread(self.parse_products)
+        await asyncio.to_thread(self.parse_products)
 
-obj = WBParser()
-obj.build_urls()
+        log.info(f"{self.sign} New products to add {len(self.list_products)}")
+        # print(self.list_products)
 
-asyncio.run(obj.parse_wb())
-
-print(obj.list_products)
 
 
