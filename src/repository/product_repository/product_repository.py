b0bdc408286fd160c3d054f33cf7ca02b5cbe783
Vladislav <talcore@yandex.ru>
2024-11-26 00:55:06 +0500

    add: Product repo

--- src/repository/product_repository/product_repository.py
+++ src/repository/product_repository/product_repository.py
@@ -1,10 +1,20 @@
 from abc import abstractmethod, ABC
+from datetime import datetime
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.repository.product_repository.product_model import Product
 
 
 class BaseRepositoryProduct(ABC):
 
     @abstractmethod
-    def add_product(self):
+    async def add_product(self, async_session, new_product: dict):
+        raise NotImplemented
+
+    @abstractmethod
+    async def add_products(self, async_session, new_products: list[dict]):
         raise NotImplemented
 
     @abstractmethod
@@ -12,7 +22,11 @@ class BaseRepositoryProduct(ABC):
         raise NotImplemented
 
     @abstractmethod
-    def get_products(self):
+    async def get_products_by_sign(self, async_session: AsyncSession, sign: str):
+        raise NotImplemented
+
+    @abstractmethod
+    async def get_all_products(self, async_session: AsyncSession):
         raise NotImplemented
 
     @abstractmethod
@@ -22,14 +36,39 @@ class BaseRepositoryProduct(ABC):
 
 class RepositoryProduct(BaseRepositoryProduct):
 
-    def add_product(self):
-        pass
+    async def add_product(self, async_session,  new_product: dict):
+
+        async with async_session() as session:
+            new_product["created_at"] = datetime.now()
+            session.add(Product(**new_product))
+            await session.commit()
+
+    async def add_products(self, async_session, new_products: list[dict]):
+
+        np = []
+
+        for p in new_products:
+            p["created_at"] = datetime.now()
+            np.append(p)
+
+        res = [Product(**p) for p in np]
+
+        async with async_session() as session:
+            session.add_all(res)
+            await session.commit()
 
     def get_product_by_id(self):
         pass
 
-    def get_products(self):
-        pass
+    async def get_products_by_sign(self, async_session: AsyncSession, sign: str):
+        query = select(Product).where(Product.provider == sign)
+        products_by_sign = await async_session.execute(query)
+        return products_by_sign.scalars().all()
+
+    async def get_all_products(self, async_session: AsyncSession):
+        query = select(Product)
+        products = await async_session.execute(query)
+        return products.scalars().all()
 
     def update_product_by_id(self):
         pass
--- src/use_cases/product_use_cases.py
+++ src/use_cases/product_use_cases.py
@@ -1,14 +1,41 @@
 from abc import abstractmethod, ABC
 
+from src.repository.product_repository.product_repository import BaseRepositoryProduct
+from settings.settings import async_session
+
 
 class BaseUseCasesProduct(ABC):
 
     @abstractmethod
-    def add_product(self):
+    async def add_product(self, new_product: dict):
+        raise NotImplemented
+
+    @abstractmethod
+    async def add_products(self, new_products: list[dict]):
+        raise NotImplemented
+
+    @abstractmethod
+    async def get_products_by_sign(self, sign: str):
+        raise NotImplemented
+
+    @abstractmethod
+    async def get_all_products(self):
         raise NotImplemented
 
 
 class UseCasesProduct(BaseUseCasesProduct):
 
-    def add_product(self):
-        pass
+    def __init__(self, repo_product: BaseRepositoryProduct):
+        self.repo_product = repo_product
+
+    async def add_product(self, new_product: dict):
+        return await self.repo_product.add_product(async_session=async_session, new_product=new_product)
+
+    async def add_products(self, new_products: list[dict]):
+        return await self.repo_product.add_products(async_session=async_session, new_products=new_products)
+
+    async def get_products_by_sign(self, sign: str):
+        return await self.repo_product.get_products_by_sign(async_session=async_session, sign=sign)
+
+    async def get_all_products(self):
+        return await self.repo_product.get_all_products(async_session=async_session)
