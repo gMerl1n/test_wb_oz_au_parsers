39a5ad07b86e19e72828be81dd07b5e94fad6fd2
Vladislav <talcore@yandex.ru>
2024-11-26 12:42:25 +0500

    add: set scheduler for all parsers

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from src.routes import _routes
 from container.di_container import di_container
+import logging
 from src.services.cookies_service.cookies_loader import oz_loader_cookies
 
 
@@ -15,27 +16,28 @@ scheduler = AsyncIOScheduler()
 @app.on_event('startup')
 async def run_wb_parser():
     w = di_container.get_parser_wb()
-    scheduler.add_job(w.parse_wb, 'cron', minute='*/5')
-    scheduler.start()
+    scheduler.add_job(w.parse_wb, 'cron', minute='*/10')
 
 
 @app.on_event('startup')
 async def run_oz_parser():
-    pass
+    oz = di_container.get_parser_oz()
+    scheduler.add_job(oz.parse_oz, 'cron', minute='*/10')
+
 
 
 @app.on_event('startup')
 async def run_au_parser():
-    pass
+    au = di_container.get_parser_au()
+    scheduler.add_job(au.parse_au, 'cron', minute='*/1')
 
 
 @app.on_event('startup')
 async def run_cookies_oz_loader():
-    scheduler.add_job(oz_loader_cookies.load_cookies, 'cron', minute='*/3')
-    scheduler.start()
+    scheduler.add_job(oz_loader_cookies.load_cookies, 'cron', minute='*/10')
 
 
-
+scheduler.start()
 
 
 
